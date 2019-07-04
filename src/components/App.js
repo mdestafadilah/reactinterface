1f62602522e379c66dcb61b6ecaabbaecccad28a
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2019-07-04 08:52:00 -0700

    Cara kedua -- inject index di state component

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,8 @@ class App extends Component {
     super();
     this.state = {
       myName: 'Desta',
-      myAppointments: []
+      myAppointments: [],
+      lastIndex: 0
     }
   }
 
@@ -21,6 +22,8 @@ class App extends Component {
       .then(response => response.json())
       .then(result => {
         const apts = result.map(item => {
+          item.aptId = this.state.lastIndex;
+          this.setState({ lastIndex: this.state.lastIndex + 1})
           return item;
         })
         this.setState({
