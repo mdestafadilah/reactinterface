41d8bc9221d39dc61f5766064dada5cf10f01943
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2019-07-04 08:08:23 -0700

    add json data sample

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,30 @@ import SearchAppointments from './SearchAppointments';
 
 // Render Sample with Class
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      myName: 'Desta',
+      myAppointments: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        });
+      });
+
+
+  }
+
   render(){
     return(
       <main className="page bg-white" id="petratings">
@@ -15,8 +39,9 @@ class App extends Component {
             <div className="col-md-12 bg-white">
               <div className="container">
                 <AddAppointments />
-                <ListAppointments />
                 <SearchAppointments />
+                <ListAppointments appointments={this.state.myAppointments} />
+
               </div>
             </div>
           </div>
